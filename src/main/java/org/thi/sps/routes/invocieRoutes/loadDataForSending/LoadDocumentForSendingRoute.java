0ebbfcbadb4e97c1d56055de97424f5dc46fd8b6
Jannik Füllgraf <jannikfuel@googlemail.com>
2024-10-16 01:41:39 +0200

    Alles auf einmal. So wie sich das gehört :D

--- src/main/java/org/thi/sps/routes/invocieRoutes/loadDataForSending/LoadDocumentForSendingRoute.java
+++ src/main/java/org/thi/sps/routes/invocieRoutes/loadDataForSending/LoadDocumentForSendingRoute.java
@@ -1,9 +1,12 @@
 package org.thi.sps.routes.invocieRoutes.loadDataForSending;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.apache.camel.Exchange;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.thi.sps.CanonicalDataModel.Document;
 import org.thi.sps.routes.generic.GenericSpiffworkflowRouteBuilder;
 
 @ApplicationScoped
@@ -26,18 +29,45 @@ public class LoadDocumentForSendingRoute extends GenericSpiffworkflowRouteBuilde
     System.out.println(redpandaUrl);
     System.out.println(documentServiceUrl);
     from("kafka:" + TOPIC_NAME + "?brokers=" + redpandaUrl)
-        .log("Message received from Kafka: ${body}")
+        .log("Message received from Kafka to load document: ${body}")
         .delay(5000)
         .removeHeaders("CamelHttp*")
         .removeHeaders("kafka*")
         .setHeader("Content-Type", constant("*/*"))
         .setHeader(Exchange.HTTP_METHOD, constant("GET"))
-        .toD(documentServiceUrl + "/api/v1/documents/" + "${body}")
-        .log("HTTP Response Code: ${header.CamelHttpResponseCode}")
-        .log("Response from product service: ${body}")
+        .log("Logging the Rest URL " + documentServiceUrl + "/api/v1/documents/" + "${body}")
+        .process(exchange -> {
+          String body = exchange.getIn().getBody(String.class);
+          body = body.replace("\"", ""); // Entferne Anführungszeichen
+          exchange.getIn().setBody(body);
+        })
+        .toD(documentServiceUrl + "/api/v1/documents/${body}")
+        .log("Response received from document service: ${body}")
+        .process(exchange -> {
+
+          String responseBody = exchange.getIn().getBody(String.class);
+          ObjectMapper objectMapperForDocument = new ObjectMapper();
+          JsonNode responseJson = objectMapperForDocument.readTree(responseBody);
+
+          String documentLink = responseJson.get("url").asText();
+          String invoiceId = responseJson.get("id").asText();
+          String documentType = "document";
+
+          Document document = new Document();
+          document.setDocumentId(invoiceId);
+          document.setInvoiceId(invoiceId);
+          document.setLinkToDocument(documentLink);
+          document.setDocumentType(documentType);
+
+          String jsonResponse = objectMapperForDocument.writeValueAsString(document);
+
+          exchange.getIn().setBody(jsonResponse);
+        })
+        .log("Response to Send To PDA: ${body}")
         .process(sendToSpiffworkflow(SPIFFWORKFLOW_MESSAGE_NAME))
         .onException(Exception.class)
-        .log("Error occurred: ${exception.message}");
+        .log("Error occurred: ${exception.message}")
+        .handled(true);
   }
 
 
